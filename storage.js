8c3f3bae290cc6cf86036f3d35dcb019277f2752
lucassa01 <164659200+lucassa01@users.noreply.github.com>
2024-11-01 09:34:01 -0300

    Delete storage.js

--- storage.js
+++
@@ -1,7 +0,0 @@
-function getPoints() {
-    return JSON.parse(localStorage.getItem("points")) || [];
-}
-
-function savePoints(points) {
-    localStorage.setItem("points", JSON.stringify(points));
-}
